ecf5ee589251c0b9fdbf90474f72b54d25f09998
Luca Foppiano <luca@foppiano.org>
2026-03-24 10:34:17 +0100

    fix: avoid NPE on malformed reference strings
    Signed-off-by: Luca Foppiano <luca@foppiano.org>

--- grobid-core/src/main/java/org/grobid/core/engines/Engine.java
+++ grobid-core/src/main/java/org/grobid/core/engines/Engine.java
@@ -1,6 +1,7 @@
 package org.grobid.core.engines;
 
 import nu.xom.Element;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.tuple.MutablePair;
 import org.apache.commons.lang3.tuple.MutableTriple;
 import org.apache.commons.lang3.tuple.Pair;
@@ -165,7 +166,7 @@ public class Engine implements Closeable {
             return finalResults;
 
         List<BiblioItem> results = parsers.getCitationParser().processingStringMultiple(references, 0);
-        if (results.size() == 0)
+        if (CollectionUtils.isEmpty(results))
             return finalResults;
 
         // consolidation in a second stage to take advantage of parallel calls
